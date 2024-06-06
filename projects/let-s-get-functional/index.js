95f31779083b4a5805c8a124d63d9abe2f99fcbc
sandrockjustin <sandrock.justin@gmail.com>
2024-06-06 15:08:15 -0500

    + attempting oldestCustomer

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -16,6 +16,7 @@ var _ = require('underbar');
  *
  * 4. To test your work, run the following command in your terminal:
  *
+ *    cd .. (until you are in the higher-most directory)
  *    npm start --prefix ./let-s-get-functional.github.io/projects/let-s-get-functional
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
@@ -23,11 +24,75 @@ var _ = require('underbar');
 
 var maleCount = function(array) {
 
+    /*
+    let count = 0;
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i]['gender'] === 'male'){
+            count += 1
+        }
+    }
+
+    return count;
+    */
+
+
+    // Professor provided this example in-class, review it for understanding
+    let males = _.filter(array, function(customer){
+        return customer.gender === 'male';
+    })
+
+    return males.length; // Returns the length of the array returned by _.filter
+
 };
 
-var femaleCount;
+var femaleCount = function(array){
 
-var oldestCustomer;
+    /*
+    let count = 0;
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i]['gender'] === 'female'){
+            count += 1
+        }
+    }
+
+    return count;
+    */
+
+    // Professor provided this example in-class, review it for understanding
+    // so basically our callback function makes an accumulator
+    // each iteration, if the conditions are true, the accumulator increases
+    // after each iteration, the return value of our callback is the current accumulator value
+    // the output return value of _.reduce is updated each iteration is equal to the return value of the callback function
+    // the return value of _.reduce is equal to output
+    let females = _.reduce(array, function(accumulator, current){
+        if (current.gender === 'female'){
+            accumulator += 1;
+        }
+        return accumulator;
+    }, 0);
+    
+    return females;
+
+};
+
+var oldestCustomer = function(array){
+
+    let oldestAge = _.reduce(array, function(previous, current){
+        if (current.age > previous){
+            console.log(previous);
+            return current.age;
+        }
+    }, 0);
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i]['age'] === oldestAge){
+            return array[i]['name'];
+        }
+    }
+
+};
 
 var youngestCustomer;
 
